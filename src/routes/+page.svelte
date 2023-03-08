c81c5949e865585c3e15ed9595174375f7d093ba
NicolasNewman <nnewman316@gmail.com>
2023-03-08 16:37:25 -0600

    added home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div id="container" class="absolute w-screen h-screen">
+	<div class="flex flex-col items-center justify-center h-[390px] m-10">
+		<h1 class="text-3xl mb-2">Kana-Learn</h1>
+		<button class="btn btn-primary mb-2 w-1/2">Learn</button>
+		<button class="btn btn-primary mb-2 w-1/2">Practice</button>
+		<button class="btn btn-primary mb-2 w-1/2">Stats</button>
+	</div>
+</div>
+
+<style>
+	#container {
+		background-image: url($lib/assets/bg-homescreen.jpg);
+	}
+</style>
