aa508ce5de7bcc86d5104da9fbe3fe7d94c89b14
NicolasNewman <nnewman316@gmail.com>
2023-03-08 18:30:47 -0600

    added pages & routing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,17 @@
 <div id="container" class="absolute w-screen h-screen">
 	<div class="flex flex-col items-center justify-center h-[390px] m-10">
 		<h1 class="text-3xl mb-2">Kana-Learn</h1>
-		<button class="btn btn-primary mb-2 w-1/2">Learn</button>
-		<button class="btn btn-primary mb-2 w-1/2">Practice</button>
-		<button class="btn btn-primary mb-2 w-1/2">Stats</button>
+		<a href="/learn" class="btn btn-primary mb-2 w-1/2">Learn</a>
+		<div class="dropdown dropdown-end mb-2 w-1/2">
+			<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+			<span tabindex="0" class="btn btn-primary w-full">Practice</span>
+			<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+			<ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+				<li><a href="/practice/hiragana">Hiragana</a></li>
+				<li><a href="/practice/katakana">Katakana</a></li>
+			</ul>
+		</div>
+		<a href="/stats" class="btn btn-primary mb-2 w-1/2">Stats</a>
 	</div>
 </div>
 
