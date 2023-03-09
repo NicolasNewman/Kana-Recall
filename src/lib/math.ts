269c69b297fa1e8118093513e1200cb66af84d46
NicolasNewman <nnewman316@gmail.com>
2023-03-09 09:54:48 -0600

    added math library

---
+++ src/lib/math.ts
@@ -0,0 +1,6 @@
+/**
+ *
+ * @param max - maximum number that can be generated
+ * @returns random number between [0, max]
+ */
+export const rand = (max: number) => Math.floor(Math.random() * (max + 1));
