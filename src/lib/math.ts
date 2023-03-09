37c6f34aebf33bba50cd3625aae0984dd9e2c0b6
NicolasNewman <nnewman316@gmail.com>
2023-03-09 11:18:03 -0600

    fixed error with rand function

--- src/lib/math.ts
+++ src/lib/math.ts
@@ -3,4 +3,4 @@
  * @param max - maximum number that can be generated
  * @returns random number between [0, max]
  */
-export const rand = (max: number) => Math.floor(Math.random() * (max + 1));
+export const rand = (max: number) => Math.floor(Math.random() * max);
