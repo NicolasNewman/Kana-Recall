f6dc13afdea3d0575cc8af220a8827c10c9b68e2
NicolasNewman <nnewman316@gmail.com>
2023-03-10 16:33:06 -0600

    adjusted practice page size

--- src/routes/practice/+page.svelte
+++ src/routes/practice/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { appWindow, LogicalSize } from '@tauri-apps/api/window';
 	import Syllabary from '../../components/form/Syllabary.svelte';
-	appWindow.setSize(new LogicalSize(900, 400));
+	appWindow.setSize(new LogicalSize(900, 556));
 </script>
 
 <div class="flex p-4">
