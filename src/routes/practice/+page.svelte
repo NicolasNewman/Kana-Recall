bee85139c9f6e11f5cc7f57c0618b262fbc1e598
NicolasNewman <nnewman316@gmail.com>
2023-03-10 10:57:47 -0600

    added practice page

--- src/routes/practice/+page.svelte
+++ src/routes/practice/+page.svelte
@@ -1,1 +1,9 @@
-<p>learn</p>
+<script lang="ts">
+	import { appWindow, LogicalSize } from '@tauri-apps/api/window';
+	import Syllabary from '../../components/form/Syllabary.svelte';
+	appWindow.setSize(new LogicalSize(900, 400));
+</script>
+
+<div class="flex p-4">
+	<Syllabary syllabary={'hira'} />
+</div>
