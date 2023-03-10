9f6f76d6ace5fa0e52084388395fc6797863fc9b
NicolasNewman <nnewman316@gmail.com>
2023-03-10 17:51:48 -0600

    styled practice page

--- src/routes/practice/+page.svelte
+++ src/routes/practice/+page.svelte
@@ -2,8 +2,33 @@
 	import { appWindow, LogicalSize } from '@tauri-apps/api/window';
 	import Syllabary from '../../components/form/Syllabary.svelte';
 	appWindow.setSize(new LogicalSize(900, 556));
+
+	let active: 'hira' | 'kata' = 'hira';
+
+	let hiragana: string[] = [];
+	let katakana: string[] = [];
 </script>
 
 <div class="flex p-4">
-	<Syllabary syllabary={'hira'} />
+	<div class="flex flex-col justify-between">
+		<ul class="menu bg-base-100 min-w-[10rem]">
+			<li>
+				<button on:click={() => (active = 'hira')} class={`${active === 'hira' ? 'active' : ''}`}
+					>Hiragana ({hiragana.length})</button
+				>
+			</li>
+			<li>
+				<button on:click={() => (active = 'kata')} class={`${active === 'kata' ? 'active' : ''}`}
+					>Katakana ({katakana.length})</button
+				>
+			</li>
+		</ul>
+		<button class="btn btn-primary">Start</button>
+	</div>
+	<div hidden={active === 'kata'}>
+		<Syllabary syllabary={'hira'} bind:selected={hiragana} />
+	</div>
+	<div hidden={active === 'hira'}>
+		<Syllabary syllabary={'kata'} bind:selected={katakana} />
+	</div>
 </div>
