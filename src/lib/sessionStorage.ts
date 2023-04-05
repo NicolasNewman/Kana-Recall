29bce5d58d6c517d24351f444166bfcb3c673cad
NicolasNewman <nnewman316@gmail.com>
2023-04-05 11:12:52 +0900

    changed null handling for getItems, added new keys

--- src/lib/sessionStorage.ts
+++ src/lib/sessionStorage.ts
@@ -1,7 +1,23 @@
+import { DefaultStoredStats, type StoredStats } from './syllabary';
+
 /** Mapping of keys to their corresponding data type */
 export interface SessionStorage {
-	keyset: string[];
+	keyset: string[] | null;
+	stats: StoredStats;
+	settings: {
+		recentStatCount: number;
+	};
 }
+
+/** Default values for each key in the sessionStorage */
+const SessionStorageDefaults: { [key in keyof SessionStorage]: SessionStorage[key] } = {
+	keyset: null,
+	stats: DefaultStoredStats,
+	settings: {
+		recentStatCount: 30
+	}
+};
+
 /** Union of keys which can be used to access the sessionStore */
 export type StorageKeys = keyof SessionStorage;
 
@@ -18,11 +34,11 @@ export function setItem<T extends StorageKeys>(key: T, data: SessionStorage[T]) 
  * Typed wrapper around sessionStorage
  * @param key predefined key
  * @param reset wheather to remove the item from the store after access. Default: false
- * @returns data belonging to that key, or null if it was never set
+ * @returns data belonging to that key, or the default value if its never been set
  */
-export function getItem<T extends StorageKeys>(key: T, reset = false): SessionStorage[T] | null {
+export function getItem<T extends StorageKeys>(key: T, reset = false): SessionStorage[T] {
 	const data = sessionStorage.getItem(key);
 	if (reset) sessionStorage.removeItem(key);
 	if (data) return JSON.parse(data) as SessionStorage[T];
-	return null;
+	return SessionStorageDefaults[key];
 }
