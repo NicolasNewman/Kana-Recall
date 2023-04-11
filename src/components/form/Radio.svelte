aedb87862ea010dfc1f21428048b7293621dcde0
NicolasNewman <nnewman316@gmail.com>
2023-04-11 13:26:48 +0900

    added radio component

---
+++ src/components/form/Radio.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	export let label: string;
+	export let value: any;
+	export let bind: string;
+	export let name: string;
+</script>
+
+<div class="form-control">
+	<label class="label cursor-pointer">
+		<span class="label-text mr-1">{label}</span>
+		<input type="radio" bind:group={bind} {name} class="radio" {value} />
+	</label>
+</div>
