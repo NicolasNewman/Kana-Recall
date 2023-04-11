8935fe76224fbb251e0a4d42a15f5d8c951a3d79
NicolasNewman <nnewman316@gmail.com>
2023-04-11 13:27:09 +0900

    added accuracy calculation helper

--- src/lib/math.ts
+++ src/lib/math.ts
@@ -1,5 +1,18 @@
+import type { Statistic } from './syllabary';
+
 /**
  * @param max - maximum number that can be generated
  * @returns random number between [0, max]
  */
 export const rand = (max: number) => Math.floor(Math.random() * max);
+
+export const accuracy = (stat: Statistic, mode: 'all' | 'recent' = 'all') => {
+	let acc = -2;
+	if (mode === 'recent') {
+		acc = stat.recent.reduce((prev, curr) => prev + curr, 0) / stat.recent.length;
+	} else {
+		acc = stat.allTime.correct / (stat.allTime.correct + stat.allTime.incorrect);
+	}
+
+	return isNaN(acc) ? -2 : acc;
+};
