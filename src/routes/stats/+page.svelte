5cd1c0d051e1f34f2cfc8c10d2964fb04e518c09
NicolasNewman <nnewman316@gmail.com>
2023-04-17 12:32:09 +0900

    added recall mode to stat page

--- src/routes/stats/+page.svelte
+++ src/routes/stats/+page.svelte
@@ -10,16 +10,27 @@
 	appWindow.setSize(new LogicalSize(1000, 600));
 
 	const parseData = () => {
-		return Object.entries(stats[selectedKana])
-			.filter(([key, value]) => value.recent.length > 0)
+		const data = Object.entries(stats[selectedKana])
+			.filter(([key, value]) =>
+				selectedMode === 'recall'
+					? value.recent.recall.length > 0
+					: value.recent.accuracy.length > 0
+			)
 			.sort((a, b) => accuracy(b[1], selectedMode) - accuracy(a[1], selectedMode));
+		return selectedMode === 'recall' ? data.reverse() : data;
 	};
 
+	type Mode = 'accuracy' | 'all' | 'recall';
 	const kana: Syllabary[] = ['hira', 'kata'];
-	const modes = ['recent', 'all'];
+	const modes: Mode[] = ['accuracy', 'all', 'recall'];
 	const stats = getItem('stats');
 	let selectedKana: Syllabary = 'hira';
-	let selectedMode: 'all' | 'recent' = 'all';
+	let selectedMode: 'all' | 'accuracy' | 'recall' = 'all';
+	const modeName: { [key in Mode]: string } = {
+		all: 'All Time',
+		accuracy: 'Accuracy',
+		recall: 'Recall'
+	};
 	let data = parseData();
 
 	$: {
@@ -43,12 +54,7 @@
 		<span class="border-b border-gray-500">Mode</span>
 
 		{#each modes as mode}
-			<Radio
-				value={mode}
-				label={mode === 'all' ? 'All Time' : 'Recent'}
-				bind:bind={selectedMode}
-				name="selectedMode"
-			/>
+			<Radio value={mode} label={modeName[mode]} bind:bind={selectedMode} name="selectedMode" />
 		{/each}
 		<a href={routes.home} class="btn btn-primary w-full mt-auto">Home</a>
 	</div>
@@ -74,8 +80,10 @@
 				// @ts-ignore
 				formatter: function (val, { dataPointIndex }) {
 					const dat = data[dataPointIndex][1];
-					if (selectedMode === 'recent') {
-						return `${val}% (${sumArray(dat.recent)}/${dat.recent.length})`;
+					if (selectedMode === 'accuracy') {
+						return `${val}% (${sumArray(dat.recent.accuracy)}/${dat.recent.accuracy})`;
+					} else if (selectedMode === 'recall') {
+						return `${val}s`;
 					}
 					return `${val}% (${dat.allTime.correct}/${dat.allTime.correct + dat.allTime.incorrect})`;
 				},
@@ -83,13 +91,19 @@
 			},
 			series: [
 				{
-					name: 'Accuracy',
-					data: data.map(([key, value]) => ({ x: key, y: (accuracy(value) * 100).toFixed(2) })),
+					name: selectedMode === 'recall' ? 'Recall' : 'Accuracy',
+					data: data.map(([key, value]) => ({
+						x: key,
+						y: (selectedMode === 'recall'
+							? accuracy(value, 'recall')
+							: accuracy(value, selectedMode) * 100
+						).toFixed(2)
+					})),
 					color: '#0f0'
 				}
 			],
 			xaxis: {
-				max: 100
+				max: selectedMode === 'recall' ? undefined : 100
 			},
 			yaxis: {
 				labels: {
