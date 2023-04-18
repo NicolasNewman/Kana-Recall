da9fe33e596b1ce7795b2391ba329af9c1372c22
NicolasNewman <nnewman316@gmail.com>
2023-04-18 12:37:52 +0900

    added basic settings page

---
+++ src/routes/settings/+page.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	import { routes } from '$lib/router';
+	import { getItem, resetSession, setItem } from '$lib/sessionStorage';
+	import { LogicalSize, appWindow } from '@tauri-apps/api/window';
+
+	appWindow.setSize(new LogicalSize(350, 500));
+
+	const settings = getItem('settings');
+
+	function save(e: SubmitEvent) {
+		const formData = new FormData(e.target as HTMLFormElement);
+		const data: any = {};
+		for (let field of formData) {
+			const [key, value] = field;
+			data[key] = value;
+		}
+		setItem('settings', data);
+	}
+
+	function reset() {
+		resetSession();
+	}
+</script>
+
+<form
+	class="flex flex-col p-6 w-screen titlebar-vh justify-between"
+	on:submit|preventDefault={save}
+>
+	<div>
+		<div class="form-control w-full max-w-xs">
+			<label class="label" for="recentStatCount">
+				<span class="label-text text-base">Number of recent answers to save</span>
+			</label>
+			<input
+				type="number"
+				name="recentStatCount"
+				placeholder="Type here"
+				class="input input-bordered w-full max-w-xs"
+				value={settings.recentStatCount}
+			/>
+		</div>
+	</div>
+	<div>
+		<a href={routes.home} class="btn btn-secondary">Cancel</a>
+		<button type="submit" class="btn btn-primary">Save</button>
+	</div>
+</form>
