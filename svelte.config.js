9d249356feb2684428c4e3d5fcf62e1b71141511
NicolasNewman <nnewman316@gmail.com>
2023-04-18 12:38:30 +0900

    set prerender path entries

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,21 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		prerender: {
+			entries: [
+				'/test/test/hira',
+				'/test/test/kata',
+				'/test/test',
+				'/test/practice/hira',
+				'/test/practice/kata',
+				'/test/practice',
+				'/',
+				'/practice',
+				'/settings',
+				'/stats'
+			]
+		}
 	},
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
