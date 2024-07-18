8231d3ec4f75b4eba2d48f2273d46e3bb67bb209
NicolasNewman <nanewman01@gmail.com>
2024-07-18 12:12:57 -0500

    feat(store): use sessionStorage only on desktop

--- frontend/src/lib/sessionStorage.ts
+++ frontend/src/lib/sessionStorage.ts
@@ -3,7 +3,7 @@ import { DefaultStoredStats, type StoredStats } from './syllabary';
 import { BaseDirectory, exists, writeTextFile, readTextFile, createDir } from '@tauri-apps/api/fs';
 
 /** Mapping of keys to their corresponding data type */
-export interface SessionStorage {
+export interface Storage {
 	keyset: string[] | null;
 	stats: StoredStats;
 	settings: {
@@ -12,10 +12,13 @@ export interface SessionStorage {
 	};
 }
 
-const sessionStorageKeys: (keyof SessionStorage)[] = ['keyset', 'settings', 'stats'];
+const storageKeys: (keyof Storage)[] = ['keyset', 'settings', 'stats'];
+
+/** desktop: sessionStorage, other: localStorage (on desktop we write to disk and only need temporary storage) */
+const storage = getPlatform() === 'desktop' ? sessionStorage : localStorage;
 
 /** Default values for each key in the sessionStorage */
-const SessionStorageDefaults: { [key in keyof SessionStorage]: SessionStorage[key] } = {
+const StorageDefaults: { [key in keyof Storage]: Storage[key] } = {
 	keyset: null,
 	stats: DefaultStoredStats,
 	settings: {
@@ -24,34 +27,34 @@ const SessionStorageDefaults: { [key in keyof SessionStorage]: SessionStorage[ke
 	}
 };
 
-/** Union of keys which can be used to access the sessionStore */
-export type StorageKeys = keyof SessionStorage;
+/** Union of keys which can be used to access the browser's store */
+export type StorageKeys = keyof Storage;
 
 /**
- * Typed wrapper around sessionStorage
+ * Typed wrapper around browser's storage api
  * @param key predefined key
  * @param data data with type matching predefined key
  */
-export function setItem<T extends StorageKeys>(key: T, data: SessionStorage[T]) {
-	sessionStorage.setItem(key, JSON.stringify(data));
+export function setItem<T extends StorageKeys>(key: T, data: Storage[T]) {
+	storage.setItem(key, JSON.stringify(data));
 }
 
 /**
- * Typed wrapper around sessionStorage
+ * Typed wrapper around browser's storage api
  * @param key predefined key
  * @param reset wheather to remove the item from the store after access. Default: false
  * @returns data belonging to that key, or the default value if its never been set
  */
-export function getItem<T extends StorageKeys>(key: T, reset = false): SessionStorage[T] {
-	const data = sessionStorage.getItem(key);
-	if (reset) sessionStorage.removeItem(key);
-	if (data) return JSON.parse(data) as SessionStorage[T];
-	return SessionStorageDefaults[key];
+export function getItem<T extends StorageKeys>(key: T, reset = false): Storage[T] {
+	const data = storage.getItem(key);
+	if (reset) storage.removeItem(key);
+	if (data) return JSON.parse(data) as Storage[T];
+	return StorageDefaults[key];
 }
 
 export function resetSession() {
-	for (const key of sessionStorageKeys) {
-		setItem(key, SessionStorageDefaults[key]);
+	for (const key of storageKeys) {
+		setItem(key, StorageDefaults[key]);
 	}
 }
 
@@ -63,9 +66,9 @@ export async function readFromDisk() {
 		if (await exists('data.conf', { dir: BaseDirectory.AppData })) {
 			const data = JSON.parse(
 				await readTextFile('data.conf', { dir: BaseDirectory.AppData })
-			) as Partial<SessionStorage>;
+			) as Partial<Storage>;
 			Object.entries(data).forEach(([key, value]) => {
-				setItem(key as keyof SessionStorage, value);
+				setItem(key as keyof Storage, value);
 			});
 		}
 	}
@@ -79,7 +82,7 @@ export async function writeToDisk() {
 		const temp = {
 			settings: getItem('settings'),
 			stats: getItem('stats')
-		} as Partial<SessionStorage>;
+		} as Partial<Storage>;
 		// BaseDir not found
 		if (!(await exists('', { dir: BaseDirectory.AppData }))) {
 			await createDir('', { dir: BaseDirectory.AppData, recursive: true });
