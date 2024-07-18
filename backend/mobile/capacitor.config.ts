178c07c9e9e18bd96d2e633c3abb5ae126d47113
NicolasNewman <nanewman01@gmail.com>
2024-07-18 18:43:23 -0500

    feat(mobile): added capacitor for mobile deployment

---
+++ backend/mobile/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+	appId: 'com.nicolasnewman.kanarecall',
+	appName: 'Kana-Recall',
+	webDir: '../../frontend/build'
+};
+
+export default config;
