90c36b033dd816cb6c2557015a22844694f4f848
NicolasNewman <nanewman01@gmail.com>
2024-07-19 16:05:27 -0500

    ci: updated release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,7 +2,10 @@
 
 case $1 in 
   major|minor|patch) 
-        if npm version $1 ; then
+        if pnpm -r version:$1 ; then
+          git add .
+          git commit -m "chore: bumped package versions"
+          pnpm version $1
           git push origin develop
           git fetch origin master:master
           git merge master
